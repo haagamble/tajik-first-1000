d1e88eca5330d1990a508471492ae84d03005ef4
haagamble <haagamble@gmail.com>
2025-08-18 23:02:04 +0500

    update for static export

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
   images: {
     unoptimized: true
   }
