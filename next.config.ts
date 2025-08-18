448d53409ee386e2608749b1cdd66e01741b4a4b
haagamble <haagamble@gmail.com>
2025-08-19 00:50:25 +0500

    Build for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/tajik-vocab-app',
   trailingSlash: true,
   images: {
     unoptimized: true
