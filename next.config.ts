48dcb8a92b3bd479c4d7440b8911ed177a60ef8b
haagamble <haagamble@gmail.com>
2025-08-30 02:53:58 +0500

    Remove basePath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
-  basePath: process.env.NODE_ENV === 'production' ? '/tajik-first-1000' : '',
+  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true
